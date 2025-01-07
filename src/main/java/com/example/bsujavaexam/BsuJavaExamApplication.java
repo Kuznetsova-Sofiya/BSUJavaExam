fa34fed2a756783c9f7dbbd3a5e131515731df84
“Kuznetsova <kuznetsova.sofiya11@gmail.com>
2025-01-07 16:05:24 +0300

    Initial commit

---
+++ src/main/java/com/example/bsujavaexam/BsuJavaExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.bsujavaexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsuJavaExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsuJavaExamApplication.class, args);
+	}
+
+}
